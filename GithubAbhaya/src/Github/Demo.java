480dcd3eef1558191bafbb373038b04cd9922e69
AbhayaChavan <123442189+AbhayaChavan@users.noreply.github.com>
2023-01-26 15:52:00 +0530

    Update Demo.java

--- GithubAbhaya/src/Github/Demo.java
+++ GithubAbhaya/src/Github/Demo.java
@@ -2,6 +2,10 @@ package Github;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("pull the program to java file");
+	}
    public static void main(String args[])
    {
 	   System.out.println("Welcome Demo");
